222bf296fd303733ac6e265e1e3e94aa2e173ff1
P-Bl4ck <34515716+P-Bl4ck@users.noreply.github.com>
2019-07-04 18:40:46 -0500

    Add files via upload

---
+++ continental-master/php/tabla.php
@@ -0,0 +1,68 @@
+
+			<table class="table">
+				  <thead class="thead-dark">
+				    <tr>
+				      <th>#</th>
+				      <th>Nombre de falla</th>
+				      <th>Descripcion</th>
+				      <th>foto</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <th scope="row m-4">1</th>
+				      <td>N. prueba 100</td>
+				      <td>se reinicia el plc</td>
+				      <th>No incluye</th>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Programa trabado</td>
+				      <td>se reinicio pc</td>
+				        <th>No incluye</th>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Programa trabado</td>
+				      <td>se reinicio pc</td>
+				        <th>No incluye</th>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Programa trabado</td>
+				      <td>se reinicio pc</td>
+				        <th>No incluye</th>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Programa trabado</td>
+				      <td>se reinicio pc</td>
+				        <th>No incluye</th>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Programa trabado</td>
+				      <td>se reinicio pc</td>
+				        <th>No incluye</th>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Programa trabado</td>
+				      <td>se reinicio pc</td>
+				        <th>No incluye</th>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Programa trabado</td>
+				      <td>se reinicio pc</td>
+				        <th>No incluye</th>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Programa trabado</td>
+				      <td>se reinicio pc</td>
+				        <th>No incluye</th>
+				    </tr>
+				  </tbody>
+			</table>
+		
